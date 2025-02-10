ad668032d3995000a3fc6b275ec4ff4de25d6789
temesgen5335 <temesgen5335@gmail.com>
2025-02-10 14:47:00 +0300

    creating footer section file

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+// components/Footer.tsx
+export default function Footer() {
+    return (
+      <footer className="py-4 text-center">
+        © {new Date().getFullYear()} Temesgen. All rights reserved.
+      </footer>
+    );
+  }
+  
