5235e1229c8723401e19970cf50112f17cfb9bb2
temesgen5335 <temesgen5335@gmail.com>
2025-02-14 14:43:58 +0300

    changin herosection to client server

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 // components/HeroSection.tsx
-import ThreeDScene from "./ThreeDScene";
+// import ThreeDScene from "./ThreeDScene";
+import {Button} from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import {Link} from "react-router-dom";
+
+import {BrowserRouter} from "react-router-dom";
 
 export default function HeroSection() {
   return (
+    <BrowserRouter>
     <section className="flex flex-col md:flex-row items-center justify-between my-8">
       <div className="w-full md:w-1/2 p-4">
         <h1 className="text-5xl font-futuristic mb-4">
@@ -11,10 +19,20 @@ export default function HeroSection() {
         <p className="text-lg">
           I’m an AI engineer and full-stack developer crafting innovative solutions that blend art and technology.
         </p>
+        <Button
+          component={Link}
+          to="/contact-me"
+          disableElevation
+          variant="contained"
+          endIcon={<ArrowForwardIcon />}
+        >
+          Lets connect
+        </Button>
       </div>
       <div className="w-full md:w-1/2 h-80 md:h-[500px]">
-        <ThreeDScene />
+        {/* <ThreeDScene /> */}
       </div>
     </section>
+    </BrowserRouter>
   );
 }
