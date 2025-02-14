bd55eee0b8efc976bc0e434b0441d3d2501226fe
temesgen5335 <temesgen5335@gmail.com>
2025-02-14 14:44:22 +0300

    layout component updat

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,7 +1,9 @@
-// components/Layout.tsx
-import { ReactNode } from "react";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+"use client";
+
+import { ReactNode, useEffect, useState } from "react";
+import { ThemeContextProvider } from "./ThemeContext"; // Import your ThemeContext
+import Navbar from "./Navbar"; 
+import "../globals.css"; // Import Tailwind styles
 
 interface LayoutProps {
   children: ReactNode;
@@ -9,10 +11,32 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="bg-darkBg text-white min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow container mx-auto p-4">{children}</main>
-      <Footer />
-    </div>
+    <ThemeContextProvider>
+      <div className="bg-white text-black dark:bg-darkBg dark:text-white min-h-screen">
+        <Navbar />
+        <main className="container mx-auto px-4">{children}</main>
+      </div>
+    </ThemeContextProvider>
   );
 }
+
+
+
+// // components/Layout.tsx Old code
+// import { ReactNode } from "react";
+// import Navbar from "./Navbar";
+// import Footer from "./Footer";
+
+// interface LayoutProps {
+//   children: ReactNode;
+// }
+
+// export default function Layout({ children }: LayoutProps) {
+//   return (
+//     <div className="bg-darkBg text-white min-h-screen flex flex-col">
+//       <Navbar />
+//       <main className="flex-grow container mx-auto p-4">{children}</main>
+//       <Footer />
+//     </div>
+//   );
+// }
