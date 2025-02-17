613cbaacc22a8440641215981e9dd1bd3d67fa5c
temesgen5335 <temesgen5335@gmail.com>
2025-02-17 15:35:07 +0300

    creating utils folder to organize sub context files

---
+++ app/components/utils/BrainModel.tsx
@@ -0,0 +1,10 @@
+// // components/BrainModel.tsx
+// "use client";
+
+// import { useGLTF } from "@react-three/drei";
+// import { Mesh } from "three";
+
+// export default function BrainModel(props: any) {
+//   const { scene } = useGLTF("/assets/ai-brain.glb");
+//   return <primitive object={scene} {...props} />;
+// }
